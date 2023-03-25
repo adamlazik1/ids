2f00041cd751a99b0b0ec1f4fbb3214002925699
alazik <alazik@redhat.com>
2023-03-25 18:01:24 +0100

    Add checks

--- src/xlazik00_xlasmi00.sql
+++ src/xlazik00_xlasmi00.sql
@@ -17,7 +17,6 @@
 
 -- zmazanie tabuliek
 
-
 DROP TABLE zakaznik CASCADE CONSTRAINTS;
 DROP TABLE pracuje CASCADE CONSTRAINTS;
 DROP TABLE vyplatna_listina CASCADE CONSTRAINTS;
@@ -33,7 +32,62 @@ DROP TABLE zamestnanec CASCADE CONSTRAINTS;
 
 DROP SEQUENCE Var_symbol_seq;
 
-
+CREATE OR REPLACE TYPE numarray IS VARRAY(10) of NUMBER;
+/
+-- Validacia cisla uctu podla https://www.kutac.cz/pocitace-a-internety/jak-poznat-spravne-cislo-uctu
+CREATE OR REPLACE FUNCTION VERIFY_BANK_ACCOUNT_NUMBER(acc_no_full IN VARCHAR2) RETURN NUMBER DETERMINISTIC IS
+    acc_no VARCHAR2(10);
+    prefix VARCHAR2(10);
+    weights numarray;
+    total NUMBER;
+    total_prefix NUMBER;
+BEGIN
+    IF (REGEXP_LIKE(acc_no_full, '^(\d{2,6}-)?\d{1,10}/\d{4}$') = FALSE) THEN
+        RETURN 0;
+    END IF;
+    weights := numarray(6, 3, 7, 9, 10, 5, 8, 4, 2, 1);
+    acc_no := REGEXP_REPLACE(acc_no_full, '\d+-|/\d+', '');
+    acc_no := LPAD(acc_no, 10, '0');
+    IF (REGEXP_LIKE(acc_no_full, '-')) THEN
+        prefix := REGEXP_REPLACE(acc_no_full, '-\d+/\d+', '');
+        prefix := LPAD(prefix, 10, '0');
+    ELSE
+        prefix := '0000000000';
+    END IF;
+    total := 0;
+    total_prefix := 0;
+    FOR i in 1 .. 10 LOOP
+        total := total + weights(i)*TO_NUMBER(SUBSTR(acc_no, i, 1));
+        total_prefix := total_prefix + weights(i)*TO_NUMBER(SUBSTR(prefix, i, 1));
+    END LOOP;
+    IF (MOD(total, 11) + MOD(total_prefix, 11) = 0) THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
+END;
+/
+-- Validacia ICA podla https://cs.wikipedia.org/wiki/Identifika%C4%8Dn%C3%AD_%C4%8D%C3%ADslo_osoby
+CREATE OR REPLACE FUNCTION VERIFY_ICO(ico IN VARCHAR2) RETURN NUMBER DETERMINISTIC IS
+    weights numarray;
+    total NUMBER;
+BEGIN
+    IF (REGEXP_LIKE(ico, '\d{8}') = FALSE) THEN
+        RETURN 0;
+    END IF;
+    -- posledne tri cislice niesu pouzite ale zabranuju nutnosti definovat novy datovy typ
+    weights := numarray(8, 7, 6, 5, 4, 3, 2, 0, 0, 0);
+    total := 0;
+    FOR i in 1 .. 7 LOOP
+        total := total + weights(i)*TO_NUMBER(SUBSTR(ico, i, 1));
+    END LOOP;
+    IF (MOD(11 - MOD(total, 11), 10) = TO_NUMBER(SUBSTR(ico, 8, 1))) THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
+END;
+/
 -------------------------
 -- vytvaranie seqvecii --
 -------------------------
@@ -59,7 +113,16 @@ CREATE TABLE zakaznik (
     Email VARCHAR2(64) NOT NULL,
     Ulica VARCHAR2(64) NOT NULL,
     Mesto VARCHAR2(64) NOT NULL,
-    PSC VARCHAR2(5) NOT NULL
+    PSC VARCHAR2(5) NOT NULL,
+
+    CONSTRAINT Zk_check_email CHECK (REGEXP_LIKE(Email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$')),
+    -- podporovane standardne formaty ceskych tel. c.:
+    -- +420 777 777 777
+    -- +420777777777
+    -- 777 777 777
+    -- 777777777
+    CONSTRAINT Zk_check_tel CHECK (REGEXP_LIKE(Tel, '(\+\d{3} ?)?(\d{3} ?){2}\d{3}')),
+    CONSTRAINT Zk_check_PSC CHECK (REGEXP_LIKE(PSC, '\d{5}'))
 );
 
 
@@ -75,6 +138,8 @@ CREATE TABLE objednavka (
     Posledna_uprava DATE,
     Specifikacia VARCHAR2(255), -- Cesta k suboru so specifikaciou
 
+    CONSTRAINT Obj_check_PSC CHECK (REGEXP_LIKE(PSC, '\d{5}')),
+
     ID_zakaznik NUMBER(10) NOT NULL,
     CONSTRAINT fk_objednavka_zakaznik FOREIGN KEY (ID_zakaznik)
     REFERENCES zakaznik(ID_zakaznik)
@@ -84,6 +149,7 @@ CREATE TABLE objednavka (
 
 -------------------------------------------
 -- Generalizacia/Specializacia
+-- Vztahy boli upravene podla spatnej vazby hodnotenia prvej casti projektu
 -- nadtyp (zamestnanec) + podtyp (externy_zamestnanec) + podtyp (vlastny_zamestnanec)
 -- nadtyp (vlastny_zamestnanec) + podtyp (povereny_pracovnik)
 -- podtypy obsahuju primarny kluc natypu
@@ -96,16 +162,26 @@ CREATE TABLE zamestnanec (
     Specializacia VARCHAR2(64) NOT NULL,
     Tel VARCHAR2(16) NOT NULL,
     Email VARCHAR2(32) NOT NULL,
-    Cislo_uctu VARCHAR(22) NOT NULL,  -- TODO: CHECK --
-    Var_symbol NUMBER(8) DEFAULT Var_symbol_seq.NEXTVAL
-);
+    Cislo_uctu VARCHAR2(22) NOT NULL,
+    Var_symbol NUMBER(8) DEFAULT Var_symbol_seq.NEXTVAL,
 
+    CONSTRAINT Zm_check_email CHECK (REGEXP_LIKE(Email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$')),
+    CONSTRAINT Zm_check_tel CHECK (REGEXP_LIKE(Tel, '(\+\d{3} ?)?(\d{3} ?){2}\d{3}')),
+
+    -- funkcie nemozu byt volane v CHECK priamo, nutnost vytvorit extra stlpec
+    Ucet_valid NUMBER(1) GENERATED ALWAYS AS (VERIFY_BANK_ACCOUNT_NUMBER(Cislo_uctu)) VIRTUAL,
+    CONSTRAINT Zm_check_cislo_uctu CHECK (Ucet_valid = 1)
+);
 
 CREATE TABLE externy_zamestnanec (
     ID_zamestnanca NUMBER(8) PRIMARY KEY,
-    ICO VARCHAR2(8), -- TODO: CHECK --
+    ICO VARCHAR2(8) NOT NULL,
     DIC VARCHAR2(12) NOT NULL,
     Nazov_firmy VARCHAR2(64) NOT NULL,
+
+    ICO_valid NUMBER(1) GENERATED ALWAYS AS (VERIFY_ICO(ICO)) VIRTUAL,
+    CONSTRAINT check_ICO CHECK(ICO_valid = 1),
+    CONSTRAINT check_DIC CHECK(DIC = CONCAT('CZ', ICO)),
 
     CONSTRAINT fk_externy_zamestnanec
     FOREIGN KEY (ID_zamestnanca)
@@ -118,7 +194,7 @@ CREATE TABLE vlastny_zamestnanec (
     ID_zamestnanca NUMBER(8) PRIMARY KEY,
     Cislo_zdravotneho_preukazu VARCHAR(32),
     Datum_narodenia DATE NOT NULL,
-    Plat_hod NUMBER(8) NOT NULL,
+    Plat_hod NUMBER(8) NOT NULL, -- predpoklada sa plat v CZK
     Uvazok VARCHAR2(16) NOT NULL,
     Dovolenka_dni NUMBER NOT NULL,
     Ulica VARCHAR2(32) NOT NULL,
@@ -127,6 +203,13 @@ CREATE TABLE vlastny_zamestnanec (
     Cislo_OP VARCHAR2(8) NOT NULL,
     Datum_nastupu DATE NOT NULL,
     Datum_ukoncenia DATE,
+
+    CONSTRAINT Zm_check_PSC CHECK (REGEXP_LIKE(PSC, '\d{5}')),
+    CONSTRAINT Zm_check_Cislo_OP CHECK (REGEXP_LIKE(Cislo_OP, '[A-Z]{2}\d{6}')),
+    -- Cislo zdravotneho preukazu nema pevne dany format kedze cudzincom je pridelene v inom formate ako ceskym obcanom
+    CONSTRAINT Zm_check_Cislo_zdravotneho_preukazu CHECK (REGEXP_LIKE(Cislo_zdravotneho_preukazu, '^\d+$')),
+    CONSTRAINT Zm_check_Dovolenka_dni CHECK (Dovolenka_dni >= 0),
+    CONSTRAINT Zm_check_Plat_hod CHECK (Plat_hod > 0),
 
     nadriadeny NUMBER REFERENCES vlastny_zamestnanec(ID_zamestnanca)
     ON DELETE SET NULL,
@@ -190,6 +273,8 @@ CREATE TABLE vybavenie (
     Datum_nakupu DATE NOT NULL,
     Nakupna_zmluva VARCHAR2(255),
 
+    CONSTRAINT Vb_check_Cena CHECK (Cena >= 0),
+
     ID_zamestnanca NUMBER(8) NOT NULL,
     CONSTRAINT fk_vybavenie_zamestnanec FOREIGN KEY (ID_zamestnanca)
     REFERENCES povereny_pracovnik(ID_zamestnanca)
@@ -209,6 +294,8 @@ CREATE TABLE material (
     Datum DATE NOT NULL,
     Nakupna_zmluva VARCHAR2(255), -- cesta k suboru
 
+    CONSTRAINT Mt_check_Cena CHECK (Cena >= 0),
+
     CONSTRAINT fk_material_objednavka FOREIGN KEY (Cislo_objednavky)
     REFERENCES objednavka(Cislo_objednavky)
     ON DELETE CASCADE,
@@ -223,18 +310,18 @@ CREATE TABLE material (
 -------------------
 
 INSERT INTO zakaznik(Priezvisko, Meno, Titul, Tel, Email, Ulica, Mesto, PSC)
-VALUES ('Kopernik', 'Mikulas', 'Ing.', '0906452987', 'kopernik@mail.com', 'Bozechtova', 'Brno', '05247');
+VALUES ('Kopernik', 'Mikulas', 'Ing.', '+421 906 452 987', 'kopernik@mail.com', 'Bozechtova', 'Brno', '05247');
 
 INSERT INTO objednavka(Ulica, Mesto, PSC, Zaciatok_vystavby, Ukoncenie_vystavby, Popis, Status, Posledna_uprava, Specifikacia, ID_zakaznik)
 VALUES ('Metodejova', 'Brno', '06484', TO_DATE('1972-07-30', 'yyyy/mm/dd'), NULL, NULL, NULL, NULL, NULL, 1);
 
 INSERT INTO zamestnanec(Priezvisko, Meno, Titul, Specializacia, Tel, Email, Cislo_uctu)
-VALUES ('Newton', 'Isac', 'Ing.', 'Statik', '0907442954', 'newton@mail.com', '1456987634');
+VALUES ('Newton', 'Isac', 'Ing.', 'Statik', '+421907442954', 'newton@mail.com', '0000111333/2700');
 INSERT INTO externy_zamestnanec(ICO, DIC, Nazov_firmy, ID_zamestnanca)
-VALUES ('13649756', '456972136', 'Stavmont', '1');
+VALUES ('25596641', 'CZ25596641', 'Stavmont', '1');
 
 INSERT INTO zamestnanec(Priezvisko, Meno, Titul, Specializacia, Tel, Email, Cislo_uctu)
-VALUES ('Tesla', 'Nicola', 'Ing.', 'Elektrika', '0907442954', 'tesla@mail.com', '1456987634');
+VALUES ('Tesla', 'Nicola', 'Ing.', 'Elektrika', '775442954', 'tesla@mail.com', '86-0199488014/0300');
 INSERT INTO vlastny_zamestnanec(Cislo_zdravotneho_preukazu, Datum_narodenia, Plat_hod, Uvazok, Dovolenka_dni, Ulica, Mesto, PSC, Cislo_OP, Datum_nastupu, Datum_ukoncenia, nadriadeny, ID_zamestnanca)
 VALUES ('1265421369', TO_DATE('1972-07-30', 'yyyy/mm/dd'), '8', 'Plny', '30', 'Ceska', 'Brno', '02354', 'HK123654', TO_DATE('1972-07-30', 'yyyy/mm/dd'), NULL, NULL, '2');
 
@@ -251,7 +338,7 @@ INSERT INTO vybavenie(Druh, Cena, Stav, Datum_nakupu, Nakupna_zmluva, ID_zamestn
 VALUES ('Bager', '6000', 'Novy', TO_DATE('1972-07-30', 'yyyy/mm/dd'), 'tu', '2');
 
 INSERT INTO material(Druh, Mnozstvo, Jednotka, Cena, Dodavatel, Datum, Nakupna_zmluva, Cislo_objednavky, ID_zamestnanca)
-VALUES ('Tehly', '20', 't', '600', 'BOUMIT', TO_DATE('1972-07-30', 'yyyy/mm/dd'), 'tu', '1', '2');
+VALUES ('Tehly', '20', 't', '600', 'BOUMIT', TO_DATE('1972-07-30', 'yyyy/mm/dd'), '/home/objednavky/1/materialy/tehly_boumit/2343242.pdf', '1', '2');
 
 
 -------------------
